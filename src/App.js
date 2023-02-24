6ba96861c624e6b57f6d435976c6c117c11e1fe0
bscsjaymar21 <namoc@gmail.com>
2023-02-24 12:03:50 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import './App.css';
 import Navbar from './Navbar'
-import {AiFillLinkedin, AiFillGithub, AiFillInstagram, AiFillMail, AiFillEnvironment, AiFillPhone} from 'react-icons/ai'
+import {AiFillFacebook, AiFillGithub,  AiFillMail, AiFillEnvironment, AiFillPhone} from 'react-icons/ai'
 
 function App() {
   return (
@@ -10,16 +10,16 @@ function App() {
     <Navbar />
 
     <section id='home' className='min-h-screen text-center mt-4 flex flex-col justify-center '>
-        <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-56 h-56 relative overflow-hidden md:h-96 md:w-96 mb-2">
+        <div className="mx-auto bg-black-500 rounded-full w-56 h-56 relative overflow-hidden md:h-96 md:w-96 mb-2">
           <img src='/me.png' layout="fill" objectFit="cover" />
         </div>
-        <h1 className='text-3xl md:text-5xl font-medium text-teal-500'>Mico Polinar</h1>
+        <h1 className='text-3xl md:text-5xl font-medium text-teal-500'>Ernest Kim Ruaza</h1>
         <h3 className='text-xl md:text-2xl'>Computer Science Student</h3>
-        <p className='text-md py-1 text-gray-800 md:text-xl'>I studied now the course of Bachelor Science in Computer Science in North Eastern Mindanao University Lianga Campus. I lived in Lianga Surigao del sur.</p>
+        <p className='text-md py-1 text-gray-800 md:text-xl'>I studied now the course of Bachelor Science in Computer Science in North Eastern Mindanao University Lianga Campus. I lived in Diatagon Lianga Surigao del sur.</p>
         <div className='text-5xl py-4 flex justify-center gap-16 text-gray-600'>
-          <AiFillLinkedin /> 
+          <AiFillFacebook /> 
           <AiFillGithub />
-          <AiFillInstagram />
+          <AiFillMail />
         </div>
     </section>
 
@@ -38,35 +38,35 @@ function App() {
     
     </section>
 
-    <section id="footer" className='mt-5 rounded bg-black py-8 px-8'>
-      <div className='text-white md:text-center'>
+    <section id="footer" className='mt-5 rounded bg-white py-8 px-8'>
+      <div className='text-black md:text-center'>
         <h1 className='font-medium text-lg'>Contact Me</h1>
         <p>Wanna talk anything?</p>
 
         <ul className='md:flex justify-center gap-4'>
           <li className='flex items-center gap-2'>
             <AiFillMail />
-            <p>micopolinar@gmail.com</p>
+            <p>ernestkimkimar@gmail.com</p>
           </li>
           <li className='flex items-center gap-2'>
             <AiFillEnvironment />
-            <p>Lianga Surigao Del Sur </p>
+            <p>Diatagon Lianga Surigao Del Sur </p>
           </li>
           <li className='flex items-center gap-2'>
             <AiFillPhone/>
-            <p>09091234567</p>
+            <p>09501886956</p>
           </li>
         </ul>
 
         <div className='flex text-xl text-white gap-4 mt-2 md:justify-center'>
-          <AiFillLinkedin /> 
+          <AiFillFacebook /> 
           <AiFillGithub />
-          <AiFillInstagram />
+          <AiFillMail />
         </div>
 
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8"/>
 
-        <p className="text-center text-sm text-gray-300 sm:text-center">
+        <p className="text-center text-sm text-black sm:text-center">
           © 2022
         
         </p>
